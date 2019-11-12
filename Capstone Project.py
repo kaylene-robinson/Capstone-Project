9fe70a3bca8be79e4ae339debd78dd99ec093a7c
kaylene-robinson <55884102+kaylene-robinson@users.noreply.github.com>
2019-11-11 21:51:56 -0500

    Add files via upload

---
+++ Capstone Project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## This notebook will be mainly used for the capstone project.
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[9]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
